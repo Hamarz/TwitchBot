d14c45422d8eeb29e9c653dba3da046da7c8afc4
Hamarz <mykkanen.tuomas@gmail.com>
2017-03-31 21:16:20 +0300

    Adding a config manager :100:
    replaces the information stored on the soure to external config file.
    also removes my old oauth tokens and db information :+1:

---
+++ Hamar/Core/ConfigManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamar.Core
+{
+    public class ConfigManager
+    {
+        private Dictionary<string, string> configproperties;
+
+        public ConfigManager()
+        {
+            configproperties = new Dictionary<string, string>();
+
+            var lines = File.ReadAllLines(@"settings.cfg");
+
+            foreach(var line in lines)
+            {
+                string key, value;
+                key = line.Split('=')[0].Trim(' ');
+                value = line.Split('=')[1].Trim(' ');
+                configproperties.Add(key, value);
+            }
+        }
+
+        private string GetValue(string key)
+        {
+            if (!configproperties.TryGetValue(key, out string value))
+                throw new Exception($"There's no property for config {key}");
+            return value;
+        }
+
+        internal uint GetUInt(string key)
+        {
+            return uint.Parse(GetValue(key));
+        }
+
+        public int GetInt(string key)
+        {
+            return int.Parse(GetValue(key));
+        }
+        public bool GetBool(string key)
+        {
+            return bool.Parse(GetValue(key));
+        }
+        public string GetString(string key)
+        {
+            return GetValue(key);
+        }
+    }
+}
--- Hamar/Global.cs
+++ Hamar/Global.cs
@@ -6,6 +6,7 @@ namespace Hamar
 {
     public static class Global
     {
+        public static ConfigManager Config { get; private set; }
         public static MysqlDatabase Database { get; private set; }
         public static RegexEngine Regex { get; private set; }
         public static Twitch.TwitchClient Twitch { get; private set; }
@@ -14,11 +15,15 @@ namespace Hamar
 
         public static void Initialize()
         {
-            Database = new MysqlDatabase("localhost", "root", "ascent", "twitchBot", 3306);
+            Config = new ConfigManager();
+            Database = new MysqlDatabase(Config.GetString("Database.Hostname"), 
+                                         Config.GetString("Database.Username"),
+                                         Config.GetString("Database.Password"), Config.GetString("Database.Database"),
+                                         Config.GetUInt("Database.Port"));
             Regex = new RegexEngine();
             Twitch = new Hamar.Twitch.TwitchClient();
             PluginManager = new PluginManager();
-            if(Twitch.Connect("xtume", "oauth:dnurocgjxbhpiyozfal66vz876kuev"))
+            if(Twitch.Connect(Config.GetString("Twitch.Username"), Config.GetString("Twitch.Password")))
                 PluginManager.Initialize();
 
 

d592238863326bc45d29823ec1ffd1a4bfe3f9ad
Hamarz <mykkanen.tuomas@gmail.com>
2017-04-01 21:18:12 +0300

    Sql for the project

---
+++ Hamar/Sql/database.sql
@@ -0,0 +1,12 @@
+﻿DROP DATABASE twitchBot;
+CREATE DATABASE twitchBot;
+USE twitchBot;
+CREATE TABLE regex_options (
+	ID int NOT NULL AUTO_INCREMENT,
+	Name varchar(255),
+	Expression varchar(255),
+	TargetMethod varchar(255),
+	PRIMARY KEY (ID)
+);
+
+
